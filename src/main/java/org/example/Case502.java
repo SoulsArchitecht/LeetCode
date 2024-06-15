0eca9bd17bea5e7de74a56aa4078b73286b42da0
Serge <sshibkojavadev@gmail.com>
2024-06-15 19:15:59 +0800

    Case_502_Solution "IPO"

---
+++ src/main/java/org/example/Case502.java
@@ -0,0 +1,87 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Case502 {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+
+        int n = profits.length;
+
+        Project[] projects = new Project[n];
+        for (int i = 0; i < n; i++) {
+            projects[i] = new Project(profits[i], capital[i]);
+        }
+        Arrays.sort(projects, (a, b) -> (a.capital - b.capital));
+
+        PriorityQueue<Project> priorityQueue = new PriorityQueue<>((a, b) -> (b.profit - a.profit));
+
+        int i = 0;
+        while (k > 0) {
+            while (i < n && projects[i].capital <= w) {
+                priorityQueue.offer(projects[i]);
+                i++;
+            }
+
+            if (!priorityQueue.isEmpty()) {
+                w += priorityQueue.poll().profit;
+                k--;
+            } else {
+                break;
+            }
+        }
+
+        return w;
+    }
+
+    static class Project {
+        int profit;
+        int capital;
+
+        public Project(int profit, int capital) {
+            this.profit = profit;
+            this.capital = capital;
+        }
+    }
+
+//Runtime - 70ms  (93.96%)
+//Memory - 56.84mb  (96.78%)
+}
+
+/**
+
+502. IPO
+ .
+        Suppose LeetCode will start its IPO soon. In order to sell a good price of its shares to Venture Capital, LeetCode would like to work on some projects to increase its capital before the IPO. Since it has limited resources, it can only finish at most k distinct projects before the IPO. Help LeetCode design the best way to maximize its total capital after finishing at most k distinct projects.
+
+        You are given n projects where the ith project has a pure profit profits[i] and a minimum capital of capital[i] is needed to start it.
+
+        Initially, you have w capital. When you finish a project, you will obtain its pure profit and the profit will be added to your total capital.
+
+        Pick a list of at most k distinct projects from given projects to maximize your final capital, and return the final maximized capital.
+
+        The answer is guaranteed to fit in a 32-bit signed integer.
+
+        Example 1:
+        Input: k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]
+        Output: 4
+        Explanation: Since your initial capital is 0, you can only start the project indexed 0.
+        After finishing it you will obtain profit 1 and your capital becomes 1.
+        With capital 1, you can either start the project indexed 1 or the project indexed 2.
+        Since you can choose at most 2 projects, you need to finish the project indexed 2 to get the maximum capital.
+        Therefore, output the final maximized capital, which is 0 + 1 + 3 = 4.
+
+        Example 2:
+        Input: k = 3, w = 0, profits = [1,2,3], capital = [0,1,2]
+        Output: 6
+
+        Constraints:
+        1 <= k <= 105
+        0 <= w <= 109
+        n == profits.length
+        n == capital.length
+        1 <= n <= 105
+        0 <= profits[i] <= 104
+        0 <= capital[i] <= 109
+
+*/
