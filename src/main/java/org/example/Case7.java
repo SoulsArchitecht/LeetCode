b8997fa79f96364276951e1be51852612e7fc0a1
Serge <sshibkojavadev@gmail.com>
2024-10-31 14:29:15 +0800

    Case_7_AddSolution "Reverse Integer"

--- src/main/java/org/example/Case7.java
+++ src/main/java/org/example/Case7.java
@@ -23,7 +23,7 @@ public class Case7 {
 
         long resultNumber = Long.parseLong(result.toString());
 
-        if (resultNumber < -2_147_483_648 || resultNumber > 2_147_483_647) {
+        if (resultNumber < Integer.MIN_VALUE || resultNumber > Integer.MAX_VALUE) {
             return 0;
         } else {
             return (int) resultNumber;
@@ -32,6 +32,31 @@ public class Case7 {
 
 //Runtime - 2ms  (14.11%)
 //Memory - 41.14mb  (11.23%)
+
+
+    public int reverse2(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + pop;
+        }
+
+        return rev;
+    }
+
+//Runtime - 0ms  (100.00%)
+//Memory - 40.51mb  (85.09%)
+
 }
 
 /**
